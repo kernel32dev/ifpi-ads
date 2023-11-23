60db7be027efd97387c7fb0558c5c5cdf7a9c588
kernel32dev <kernel32dev@gmail.com>
2023-11-23 14:31:46 -0300

    removido controller e view da classe Pessoa

--- adp/gerar_controllers.bat
+++ adp/gerar_controllers.bat
@@ -1,4 +1,3 @@
 dotnet-aspnet-codegenerator controller -name AlunoController -m Aluno -dc MyDbContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries
-dotnet-aspnet-codegenerator controller -name PessoaController -m Pessoa -dc MyDbContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries
 dotnet-aspnet-codegenerator controller -name ProfessorController -m Professor -dc MyDbContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries
 dotnet-aspnet-codegenerator controller -name UsuarioController -m Usuario -dc MyDbContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries
